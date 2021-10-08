4ca8ce8f52bc73027b1bd11b2ed5ab8d99e754ea
PatrickKvrgic <patrikkvrgic@gmail.com>
2021-10-08 12:16:44 +0200

    Added up to Vaja 7

--- RekurzijaVaja/RekurzijaVaja/Program.cs
+++ RekurzijaVaja/RekurzijaVaja/Program.cs
@@ -14,6 +14,10 @@ namespace RekurzijaVaja
             Console.WriteLine("2 na 10 je " + PotencaRekurzivno(10));
             Console.WriteLine("7 Fibonaccijevo stevilo je " + Fib(7));
             Console.WriteLine("5 stevilo je " + Vaja3(5));
+            Console.WriteLine("5 stevilo je " + Vaja4(5));
+            Console.WriteLine("5 stevilo je " + Vaja5(5));
+            Console.WriteLine("5 stevilo je " + Vaja6(5));
+            Console.WriteLine("5 stevilo je " + Vaja7(5));
             Console.ReadLine();
         }
         //Primer Iterativno
@@ -47,5 +51,38 @@ namespace RekurzijaVaja
                 return 2;
             return Vaja3(n - 2) * Vaja3(n - 1) - 1;
         }
-    }
+        //Vaja 4
+        static int Vaja4(int n)
+        {
+            if (n == 1)
+                return 2;
+            return 3 * Vaja4(n - 1) + 2;
+        }
+        //Vaja 5
+        static int Vaja5(int n)
+        {
+            if (n == 1)
+                return 1;
+            if (n == 2)
+                return 2;
+            return Vaja5(n - 1) * 2 + Vaja5(n - 2);
+        }
+        //Vaja 6
+        static int Vaja6(int n)
+        {
+            if (n == 1)
+                return 1;
+            if (n == 2)
+                return 4;
+            return Vaja6(n - 1) + 2;
+        }
+        //Vaja 7
+        static int Vaja7(int n)
+        {
+            if (n == 1)
+                return 1;
+            if (n == 2)
+                return 3;
+            return 2 * Vaja7(n - 1) + 2;
+        }
 }

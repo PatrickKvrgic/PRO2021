60a19748645aebfbe180d8abbd8a3dec48c4f354
PatrickKvrgic <patrikkvrgic@gmail.com>
2021-10-12 09:21:36 +0200

    Figured out fractals lmaoo woooo

--- Fractals/Fractals/Form1.cs
+++ Fractals/Fractals/Form1.cs
@@ -35,8 +35,8 @@ namespace Fractals
         {
             double oldX = x;
             double oldY = y;
-            x = x + Math.Cos(alpha * Math.PI / 180);
-            y = y + Math.Sin(alpha * Math.PI / 180);
+            x = x + d *Math.Cos(alpha * Math.PI / 180);
+            y = y + d *Math.Sin(alpha * Math.PI / 180);
             int x1 = drawX(oldX);
             int y1 = drawY(oldY);
             int x2 = drawX(x);
@@ -45,13 +45,81 @@ namespace Fractals
             g.DrawLine(p, x1, y1, x2, y2);
         }
 
+        public void Koch(int n, double step, Graphics g)
+        {
+            if (n == 0)
+            {
+                Movement(step, g);
+                return;
+            }
+            Koch(n - 1, step, g);
+            TurnLeft(60);
+            Koch(n - 1, step, g);
+            TurnLeft(-120);
+            Koch(n - 1, step, g);
+            TurnLeft(60);
+            Koch(n - 1, step, g);
+        }
+
+        public void Drevo(int n, double x, double y, double a, double dolžina, Graphics g)
+        {
+            int kot = 30;
+            double pojemek = 0.5;
+            double cx = x + dolžina * Math.Cos(a * Math.PI / 180);
+            double cy = y + dolžina * Math.Sin(a * Math.PI / 180);
+            int x1 = drawX(cx);
+            int y1 = drawY(cy);
+            int x2 = drawX(x);
+            int y2 = drawY(y);
+
+            Random r = new Random();
+            Color c = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+
+            Pen p = new Pen(c, (float)Math.Pow(n, r.Next(2)));
+            g.DrawLine(p, x1, y1, x2, y2);
+            if (n == 0)
+            {
+                return;
+            }
+            Drevo(n - 1, cx, cy, a - kot, dolžina * pojemek, g);
+            Drevo(n - 1, cx, cy, a + kot, dolžina * pojemek, g);
+            Drevo(n - 1, cx, cy, a, dolžina * (1 - pojemek), g);
+        }
+
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            x = 0.5;
-            y = 0.5;
-            alpha = 0;
-            Movement(0.5, g);
+            //x = 0.2;
+            //y = 0.9;
+            //alpha = 0;
+            //for (int k = 0; k < 4; k++)
+            //{
+            //    double step = 0.1;
+            //    Movement(step, g);
+            //    TurnLeft(-360 / 4);
+            //}
+
+            //Random r = new Random();
+            //x = 0.5;
+            //y = 0.5;
+            //alpha = 0;
+            //for (int k = 0; k < 10000; k++)
+            //{
+            //    TurnLeft(r.Next(360));
+            //    Movement(0.01, g);
+            //}
+
+            //x = 0.3;
+            //y = 0.9;
+            //for (int k = 0; k < 6; k++)
+            //{
+            //    int n = 6;
+            //    Koch(n, 1 / Math.Pow(3, n) * 0.1, g);
+            //    TurnLeft(-360 / n);
+            //}
+
+            //int n = 10;
+            //Drevo(n, 0.5, 0, 90, 0.5, g);
         }
     }
 }
